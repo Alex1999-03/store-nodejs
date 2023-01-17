0f9498545b47c0b5ff62033463d46e532546bf9f
Alex1999-03 <miguel.alexander.rodriguez1999@gmail.com>
2023-01-17 14:46:17 -0600

    style: formatted document

--- src/middlewares/schemaValidator.middleware.ts
+++ src/middlewares/schemaValidator.middleware.ts
@@ -4,7 +4,11 @@ import { StatusCode } from "../utils/enums";
 
 export const schemaValidator = (schema: AnyZodObject): RequestHandler => {
   return async (req, res, next) => {
-    const result = await schema.safeParseAsync({ params: req.params, body: req.body, files: req.files });
+    const result = await schema.safeParseAsync({
+      params: req.params,
+      body: req.body,
+      files: req.files,
+    });
     if (!result.success) {
       return res.status(StatusCode.BAD_REQUEST).json(
         result.error.issues.map((err) => ({
