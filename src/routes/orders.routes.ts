550d91b07f00b8372260c334809cdfd8a638cfc6
Alex1999-03 <miguel.alexander.rodriguez1999@gmail.com>
2023-01-18 19:24:52 -0600

    docs: added documentation for order endpoint

--- src/routes/orders.routes.ts
+++ src/routes/orders.routes.ts
@@ -12,9 +12,139 @@ import {
 
 const router = Router();
 
+/**
+ * @swagger
+ *  components:
+ *    schemas:
+ *      Items:
+ *        type: object
+ *        properties:
+ *          name:
+ *            type: string
+ *          price:
+ *            type: number
+ *          quantity:
+ *            type: number
+ *      Order: 
+ *        type: object
+ *        properties:
+ *          id: 
+ *            type: string
+ *          customer:
+ *            type: string
+ *          total: 
+ *            type: number
+ *          isCancel: 
+ *            type: boolean
+ *          items:
+ *            type: array
+ *            items:
+ *              $ref: '#/components/schemas/Items'
+ *        required:
+ *          - customer
+ *          - total
+ *          - isCancel
+ *          - items
+ * 
+ *    parameters:
+ *      orderId:
+ *        in: path
+ *        name: id
+ *        required: true
+ *        schema:
+ *          type: string
+ * 
+ *    responses:
+ *      OrderNotFound:
+ *        description: The order does not exist.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message: 
+ *                  type: string
+ *      OrderBadRequest:
+ *        description: Some order properties are invalid.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ */
+
+/**
+ * @swagger
+ * tags:
+ *  name: Orders
+ *  description: Orders endpoints
+ */
+
+/**
+ * @swagger
+ * /api/orders:
+ *  get:
+ *    summary: Get a list of orders.
+ *    tags: [Orders]
+ *    responses:
+ *      200:
+ *        description: The list of orders.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Order'
+ */
+
 router.get("/", orderController.getOrders);
 
+/**
+ * @swagger
+ * /api/orders/{id}:
+ *  get:
+ *    summary: Get a order by Id.
+ *    tags: [Orders]
+ *    parameters:
+ *      - $ref: '#/components/parameters/orderId'
+ *    responses:
+ *      200:
+ *        description: Returns a order if found.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Order'
+ *      404: 
+ *        $ref: '#/components/responses/OrderNotFound'
+ *        
+ */
+
 router.get("/:id", schemaValidator(GetOrderSchema), orderController.getOrder);
+
+/**
+ * @swagger
+ * /api/orders/add:
+ *  post:
+ *    summary: Create a order.
+ *    tags: [Orders]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Order'
+ *    responses:
+ *      201:
+ *        description: Create a order.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Order'
+ *      400:
+ *        $ref: '#/components/responses/OrderBadRequest'
+ */
 
 router.post(
   "/add",
@@ -24,6 +154,33 @@ router.post(
   orderController.postOrder
 );
 
+/**
+ * @swagger
+ * /api/orders/update/{id}:
+ *  put:
+ *    summary: Update a order.
+ *    tags: [Orders]
+ *    parameters:
+ *      - $ref: '#/components/parameters/orderId'
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Order'
+ *    responses:
+ *      200:
+ *        description: Returns the updated order.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Order'
+ *      400:
+ *        $ref: '#/components/responses/OrderBadRequest'
+ *      404: 
+ *        $ref: '#/components/responses/BrandNotFound'
+ */
+
 router.put(
   "/update/:id",
   schemaValidator(EditOrderSchema),
@@ -31,6 +188,25 @@ router.put(
   rolesValidator([Roles.ADMIN, Roles.CUSTOMER]),
   orderController.putOrder
 );
+
+/**
+ * @swagger
+ * /api/orders/delete/{id}:
+ *  delete:
+ *    summary: Delete a order.
+ *    tags: [Orders]
+ *    parameters:
+ *      - $ref: '#/components/parameters/orderId'
+ *    responses:
+ *      200:
+ *        description: Returns the deleted order.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Order'
+ *      404: 
+ *        $ref: '#/components/responses/OrderNotFound'
+ */
 
 router.delete(
   "/delete/:id",
